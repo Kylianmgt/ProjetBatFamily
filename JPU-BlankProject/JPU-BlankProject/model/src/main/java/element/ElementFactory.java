26fab6b8d9a0de29aa7e838ada09785a02739d3f
Marcus Belmont <marcus.belmont@etu.unilim.fr>
2019-05-28 14:25:18 +0200

    ElementFactory created

---
+++ JPU-BlankProject/JPU-BlankProject/model/src/main/java/element/ElementFactory.java
@@ -0,0 +1,23 @@
+package element;
+
+public class ElementFactory {
+	public Dirt createDirt(){
+		return new Dirt();		
+	}
+	public Rock createRock(){
+		return new Rock();		
+	}
+	public Nothing createNothing(){
+		return new Nothing();		
+	}
+	public Block createBlock(){
+		return new Block();		
+	}
+	
+	public Player createPlayer(){
+		return new Player();		
+	}
+	public Monster createMonster(){
+		return new Monster();		
+	}
+}
