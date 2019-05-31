a6aae192a93967896b38c127489e4aff28f4dfee
Marcus Belmont <marcus.belmont@etu.unilim.fr>
2019-05-31 13:46:20 +0200

    rocks can be pushed

--- JPU-BlankProject/JPU-BlankProject/model/src/main/java/Behaviors/IBlock.java
+++ JPU-BlankProject/JPU-BlankProject/model/src/main/java/Behaviors/IBlock.java
@@ -5,6 +5,7 @@ package Behaviors;
 import Utility.BagOfPossiblePositions;
 import Utility.Direction;
 import element.Map;
+import element.Player;
 
 
 public interface IBlock extends IBehavior {
--- JPU-BlankProject/JPU-BlankProject/model/src/main/java/element/Rock.java
+++ JPU-BlankProject/JPU-BlankProject/model/src/main/java/element/Rock.java
@@ -15,8 +15,16 @@ public class Rock extends FallingElement implements IBePushed{
 	@Override
 	public boolean interaction( Direction direction, Map map, BagOfPossiblePositions bag, Player player){
 		int[] vecteurDir = this.convertDirectionIntoInt(direction);
+		if (isNotOutOfBounds(map, this.getPositionElement().getX()+vecteurDir[0], this.getPositionElement().getY())){
+			if (map.getNiveau()[this.getPositionElement().getX()+vecteurDir[0]][this.getPositionElement().getY()].getClass()==Nothing.class){
+				map.setNiveau(new Nothing(), this.getPositionElement());
+				this.getPositionElement().setX(this.getPositionElement().getX()+vecteurDir[0]);
+				map.setNiveau(this, this.getPositionElement());
+				return true;
+			}
+		}
 		
-	return true;		
+	return false;		
 }
 
 
